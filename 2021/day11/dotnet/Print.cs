d27942bd482aab4d7351776a121f2d08176ed6b2
Edd Sansome <edwardsansome@gmail.com>
2021-12-12 23:05:44 +0000

    little tidy up

--- 2021/day11/dotnet/Print.cs
+++ 2021/day11/dotnet/Print.cs
@@ -1,13 +1,15 @@
 ﻿public class Print
 {
-    public static void Octopi(int[,] octopiGrid, int height, int width)
+  public static void Octopi(int[,] octopiGrid, int height, int width)
+  {
+    for (int h = 0; h < height; h++)
     {
-        for (var h = 0; h < height; h++)
-        {
-            Console.Write('\n');
-            for (var w = 0; w < width; w++) Console.Write(octopiGrid[h, w].ToString().PadRight(3, ' '));
-        }
-
-        Console.Write('\n');
+      Console.Write('\n');
+      for (int w = 0; w < width; w++)
+      {
+        Console.Write(octopiGrid[h, w].ToString().PadRight(3, ' '));
+      }
     }
+    Console.Write('\n');
+  }
 }
