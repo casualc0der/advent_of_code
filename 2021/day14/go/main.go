e2833885e3c85f23f2d8efcc981051e6b57c4310
Edd Sansome <edward.sansome@smartpension.co.uk>
2021-12-17 13:02:05 +0000

    day14 q1

--- 2021/day14/go/main.go
+++ 2021/day14/go/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"os"
 	"strings"
 )
 
@@ -9,29 +11,34 @@ func main() {
 
 	pairInsertionRule := map[string]string{}
 
-	sample := `NNCB
+	// sample := `NNCB
 
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C`
+	// CH -> B
+	// HH -> N
+	// CB -> H
+	// NH -> C
+	// HB -> C
+	// HC -> B
+	// HN -> C
+	// NN -> C
+	// BH -> H
+	// NC -> B
+	// NB -> B
+	// BN -> B
+	// BB -> N
+	// BC -> B
+	// CC -> N
+	// CN -> C`
 
-	raw := strings.Split(sample, "\n\n")
+	input, _ := os.ReadFile("input14.txt")
 
-	sequence := raw[0]
-	rawDictionary := strings.Split(raw[1], "\n")
+	// sample question
+	// raw := strings.Split(sample, "\n\n")
+	// question 1
+	raw := strings.Split(string(input), "\n\n")
+
+	sequence := strings.TrimSpace(raw[0])
+	rawDictionary := strings.Split(strings.TrimSpace(raw[1]), "\n")
 
 	for _, line := range rawDictionary {
 
@@ -40,17 +47,100 @@ CN -> C`
 
 	}
 
-	for i := 0; i < 1; i++ {
+	// question 1
+	question1(sequence, pairInsertionRule)
+	// question 2
+	question2(sequence, pairInsertionRule)
+
+}
+
+func question1(sequence string, rules map[string]string) {
+	for i := 0; i < 10; i++ {
 		var tempSequence string
 		for j := 0; j < len(sequence)-1; j++ {
 
 			key := string(sequence[j]) + string(sequence[j+1])
-			str := string(sequence[j]) + pairInsertionRule[key]
+			str := string(sequence[j]) + rules[key] + string(sequence[j+1])
+			tempSequence = strings.TrimSuffix(tempSequence, string(sequence[j]))
 			tempSequence += str
 
 		}
 		sequence = tempSequence
 	}
-	fmt.Println(sequence)
+	ans1 := countElements(sequence)
+
+	fmt.Printf("The answer to question 1 is: %d\n", ans1)
+
+}
+
+func question2(seq string, rules map[string]string) {
+	// how can we speed this up?
+	// find all the elements and then merge into array?
+
+	sequence := strings.Split(seq, "")
+
+	for i := 0; i < 20; i++ {
+		tempSequence := []string{}
+
+		for j := 0; j < len(sequence)-1; j++ {
+
+			key := string(sequence[j]) + string(sequence[j+1])
+			tempSequence = append(tempSequence, rules[key])
+
+		}
+
+		// ok so we have the array of the sequence now we need to merge?
+
+		// merge has to happen before this!
+
+		sequence = merge(sequence, tempSequence)
+		fmt.Println(len(sequence))
+	}
+	// ans2 := countElements(sequence)
+
+	// fmt.Printf("The answer to question 2 is: %d\n", ans2)
+
+}
+
+// this is very slow
+func merge(keep []string, merge []string) []string {
+
+	for i := range merge {
+
+		keep = append(keep[:i+1], keep[i:]...)
+	}
+	for i, derp := range merge {
+		keep[i] = derp
+	}
+
+	return keep
+
+}
+
+func countElements(sequence string) int {
+
+	elements := map[string]int{}
+
+	for _, element := range sequence {
+		if _, ok := elements[string(element)]; ok {
+			elements[string(element)]++
+		} else {
+			elements[string(element)] = 1
+		}
+	}
+
+	// find min/max
+	min := math.MaxInt32
+	max := math.MinInt32
+	for _, v := range elements {
+
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	return max - min
 
 }
